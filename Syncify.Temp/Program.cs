b5b0fb55fc701f965cb931ba5521708bb9b7df40
Yoav Levy <yoavlevy75@gmail.com>
2026-05-19 20:20:06 +0300

    lots of pings

--- Syncify.Temp/Program.cs
+++ Syncify.Temp/Program.cs
@@ -1,10 +1,21 @@
 ﻿using System.Net;
 using System.Net.NetworkInformation;
 
-var ip = "192.168.1.45";
+var ip = "192.168.1.113";
 
 var ping = new Ping();
 
-var result = await ping.SendPingAsync(IPAddress.Parse(ip));
+var roundTrips = new List<long>();
 
-Console.WriteLine(result.RoundtripTime);
+while (true)
+{
+    var result = await ping.SendPingAsync(IPAddress.Parse(ip));
+    if (result.Status == IPStatus.Success)
+    {
+        roundTrips.Add(result.RoundtripTime);
+    }
+
+    Console.WriteLine($"Current roundtrip time: {result.RoundtripTime:000}, Average: {roundTrips.Average():0.00}");
+    
+    await Task.Delay(1);
+}
